9340b5974c41a1d121bdf1349eb7dd5031358d6e
Farhodnazarov <nazarovfarhod0022@gmail.com>
2025-09-19 10:52:54 +0500

    added state function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("Farxod");
+
+  const changeName = () => {
+    if (name === "Farrux") {
+      setName("Farxod");
+    } else {
+      setName("Farrux");
+    }
+    console.log(name);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={changeName}>Change Name</button>
     </div>
   );
 }
